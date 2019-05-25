511afbb4190d208202113fecf6b3c4c04a962c16
FIXIT3491 <fixit3491@gmail.com>
2019-05-25 11:59:54 -0700

    updated wishlist files, java script for add gift

--- js/wishlist.js
+++ js/wishlist.js
@@ -2,3 +2,18 @@ function checkLower() {
     var LowerCost = document.getElementById("LowerCost").value;
     document.getElementById("LowerCost").innerHTML = x;
   }
+
+function Show() {
+  document.getElementById('form').style.visibility = "visible";
+  document.getElementById('addgiftimage').style.visibility = "hidden";
+}
+
+function ShowPlus(){
+  document.getElementById('addgiftimage').style.visibility = "visible";
+}
+
+function AddGift(addgiftname){
+  document.getElementById('namestoredgift').innerHTML = document.getElementById("addgiftname").value;
+  document.getElementById('descriptionstoredgift').innerHTML = document.getElementById("addDescription").value;
+}
+
