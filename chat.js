90060d20fa9f93c97ebb2d7a701ebea8edf85992
hightechu2 <hightechu2@uvic.ca>
2019-05-25 11:29:36 -0700

    #

--- chat.js
+++ chat.js
@@ -2,3 +2,9 @@ function updateChat(chat) {
     document.getElementById('chatarea').innerHTML = document.getElementById('chatarea').innerHTML + "<br>" + chat;
     document.getElementById("textbox").value = "";
 }
+function updateChat2(chat) {
+    if(event.keyCode === 13) {
+         document.getElementById('chatarea').innerHTML = document.getElementById('chatarea').innerHTML + "<br>" + chat;
+         document.getElementById("textbox").value = "";
+    }
+}
