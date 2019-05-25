41aa5e030485674afa08dcf0855281e0d28cd8c1
FIXIT3491 <fixit3491@gmail.com>
2019-05-25 15:53:50 -0700

    javascript can add gifts,need2merge with topbar+backend

--- js/wishlist.js
+++ js/wishlist.js
@@ -1,4 +1,4 @@
-function checkLower() {
+function checkLower(x) {
     var LowerCost = document.getElementById("LowerCost").value;
     document.getElementById("LowerCost").innerHTML = x;
   }
@@ -8,12 +8,29 @@ function Show() {
   document.getElementById('addgiftimage').style.visibility = "hidden";
 }
 
-function ShowPlus(){
-  document.getElementById('addgiftimage').style.visibility = "visible";
-}
 
-function AddGift(addgiftname){
+function AddmhghjGift(addgiftname){
   document.getElementById('namestoredgift').innerHTML = document.getElementById("addgiftname").value;
   document.getElementById('descriptionstoredgift').innerHTML = document.getElementById("addDescription").value;
+  document.getElementById('storedCost').innerHTML = document.getElementById("LowerCost").value + "-" + document.getElementById("UpperCost").value + "$";
+  document.getElementById('addgiftimage').style.visibility = "visible";
+  document.getElementById('form').style.visibility = "hidden";
 }
 
+/* with the create element function, should replace AddGift in uses when it works and Add gift be changed to edit gift*/ 
+function AddGift(addgiftname){
+  var Gift_Box = document.createElement("div");
+  Gift_Box.setAttribute("class", "storedgift");
+  
+  var newName = document.createElement("h2");
+  newName.innerText = document.getElementById("addgiftname").value;
+  var newDesc = document.createElement("h4");
+  newDesc.innerText = document.getElementById("addDescription").value;
+  var newCost = document.createElement("h4");
+  newCost.innerText = document.getElementById("LowerCost").value + "-" + document.getElementById("UpperCost").value + "$";
+
+  Gift_Box.appendChild(newName);
+  Gift_Box.appendChild(newDesc);
+  Gift_Box.appendChild(newCost);
+  document.getElementById('giftcontainer').appendChild(Gift_Box);
+}
